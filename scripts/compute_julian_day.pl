b09cff57f5ef283341f2f1792ba8f774723add9f
U-INTRANET\antonio.fazan <antonio.fazan@DF00005021286.intranet>
2015-11-03 17:32:07 -0200

    Updating package tools

--- scripts/compute_julian_day.pl
+++ scripts/compute_julian_day.pl
@@ -1,5 +1,14 @@
 #!/usr/bin/perl -w
 
+use File::Basename;
+
+if( @ARGV != 3 ) {
+
+  print "\nUsage:\n";
+  print sprintf( "\t%s <DAY> <MONTH> <YEAR>\n", basename( $0 ) );
+  exit( -1 );
+}
+
 ( $day, $month, $year ) = @ARGV;
 
 @days = ( 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 );
