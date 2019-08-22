010b9219a1afd44ecd5df032cfaf763aa642735c
ajfazan <ajfazan@gmail.com>
2019-08-22 16:45:35 -0300

    Source code for 'raster2points.py' updated

--- scripts/raster2points.py
+++ scripts/raster2points.py
@@ -42,10 +42,48 @@ def createAffineTransform( params ):
 
   assert( len( params ) == 6 )
 
-  a = np.array( [ [ params[1], params[2] ], [ params[4], params[5] ] ] )
-  b = np.array( [ [ params[0] ], [ params[3] ] ] )
+  a = np.array( [ [ params[1], params[2] ], [ params[4], params[5] ] ], dtype = np.float64 )
+  b = np.array( [ [ params[0] ], [ params[3] ] ], dtype = np.float64 )
 
   return ( a, b )
+
+def interpolateValue( px, band, null ):
+
+  assert( len( px ) == 2 )
+
+  ( dx, x ) = math.modf( px[0] )
+  ( dy, y ) = math.modf( px[1] )
+
+  i = int( y )
+  j = int( x )
+
+  u = i + 1
+  v = j + 1
+
+  g = dict()
+
+  t = band[i][j]
+  if t != null:
+    g[1] = t
+
+  t = band[i][v]
+  if t != null:
+    g[2] = t
+
+  t = band[u][j]
+  if t != null:
+    g[3] = t
+
+  t = band[u][v]
+  if t != null:
+    g[4] = t
+
+  if len( g ) == 4:
+    ga = dx * ( g[2] - g[1] ) + g[1]
+    gb = dx * ( g[4] - g[3] ) + g[3]
+    return ( dy * ( gb - ga ) + ga )
+
+  return null;
 
 def main( args ):
 
@@ -54,40 +92,53 @@ def main( args ):
   raster = openRaster( args.raster_file )
   vector = openVector( args.vector_file )
 
-  band = raster.GetRasterBand( args.band )
-  null = band.GetNoDataValue()
-
-  ( a, b ) = createAffineTransform( raster.GetGeoTransform() )
-
-  i = np.linalg.inv( a )
-
-  data = band.ReadAsArray()
-
   layer = vector.GetLayer()
-  for feature in layer:
-    geom = feature.GetGeometryRef()
-    point = geom.GetPoint()
-    # compute inverse affine transformation for each point
-    px = np.matmul( i, np.array( [ [ point[0] ], [ point[1] ] ] ) - b )
 
-    print ( px[0][0], px[1][0] )
+  if layer.GetGeomType() = wkbPoint:
+    band = raster.GetRasterBand( args.band )
+    null = band.GetNoDataValue()
 
-    # print point
-    # print feature.GetField( "ALT_ORTO" )
+    ( a, b ) = createAffineTransform( raster.GetGeoTransform() )
 
-    # retrieve values from data
-    # compute interpolated value
-    # write value into created or existing field
+    i = np.linalg.inv( a )
 
-  layer.ResetReading()
+    data = band.ReadAsArray()
 
-  # print ( h.RasterYSize, h.RasterXSize )
-  # print h.RasterCount
-  # print null
+    fields = []
+    table = layer.GetLayerDefn()
 
-  band = None
+    for k in range( table.GetFieldCount() ):
+      fields.append( table.GetFieldDefn( k ).name )
+
+    if not( args.field in fields ):
+      field = ogr.FieldDefn( args.field, ogr.OFTReal )
+      field.SetWidth( 16 )
+      field.SetPrecision( 12 )
+      layer.CreateField( field )
+
+    for feature in layer:
+      geom = feature.GetGeometryRef()
+      point = geom.GetPoint()
+      # compute inverse affine transformation for each point
+      px = np.matmul( i, np.array( [ [ point[0] ], [ point[1] ] ] ) - b )
+
+      v = interpolateValue( ( px[0][0], px[1][0] ), data, null )
+
+      if v != null:
+        # write interpolated value into created or existing field
+        feature.SetField( args.field, v )
+        layer.SetFeature( feature )
+
+    layer.SyncToDisk()
+
+    band = None
+
+  else:
+    print "Geometry type of input layer must be single point"
+
   raster = None
-  vector = None
+
+  vector.Destroy()
 
   return 0
 
