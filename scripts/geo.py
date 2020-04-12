95ed6c967154329f73373186014b612d4d379dbf
ajfazan <ajfazan@gmail.com>
2020-04-12 20:39:19 -0300

    Code refactoring for bug fixing for module 'geo'

--- scripts/geo.py
+++ scripts/geo.py
@@ -17,98 +17,117 @@ class raster:
     for k in range( 1, 1 + self.handle_.RasterCount ):
       band = self.handle_.GetRasterBand( k )
       nodata = band.GetNoDataValue()
-      if nodata is None:
+      if not( nodata is None ):
         self.band_nodata_[k] = nodata
 
-    def LoadBand( self, num ):
-      """
-      Load a raster band into an array
-      """
+  def LoadBand( self, num ):
+    """
+    Load a raster band into an array
+    """
 
-      if num <= self.handle_.RasterCount:
-        band = self.handle_.GetRasterBand( num )
-        return band.ReadAsArray()
+    if num <= self.handle_.RasterCount:
+      band = self.handle_.GetRasterBand( num )
+      return band.ReadAsArray()
 
+    return None
+
+  def GetBandCount( self ):
+    """
+    Get the number of bands in a raster dataset
+    """
+
+    return self.handle_.RasterCount
+
+  def GetBandList( self ):
+    """
+    Get a list of indices for all bands in a raster dataset
+    """
+
+    return list( range( 1, self.handle_.RasterCount + 1 ) )
+
+  def GetBandNoData( self, num ):
+    """
+    Get the nodata value (if exists) corresponding to a raster band indexed by num
+    """
+
+    nodata = None
+
+    if num <= self.handle_.RasterCount:
+      try:
+        nodata = self.band_nodata_[num]
+      except:
+        print( sys.stderr, "Raster band [%d] has no defined nodata value" % num )
+
+    return nodata
+
+  def GetBandInfo( self, num ):
+    """
+    Get basic band statistics
+    """
+
+    if num > self.handle_.RasterCount:
+      sys.exit( "Index [{0}] out of bounds".format( num ) )
       return None
 
-    def GetBandNoData( self, num ):
-      """
-      Get the nodata value (if exists) corresponding to a raster band indexed by num
-      """
+    info = {}
 
-      nodata = None
+    info['nodata'] = self.GetBandNoData( num )
 
-      if num <= sel.handle_.RasterCount:
-        try
-          nodata = self.nodata_[num]
-        except:
-          print( sys.stderr, "Raster band [%d] has no defined nodata value", num )
+    band = self.handle_.GetRasterBand( num )
 
-      return nodata
+    min_dn = band.GetMinimum()
+    max_dn = band.GetMaximum()
 
-    def GetBandInfo( self, num ):
-      """
-      Get basic band statistics
-      """
+    if not min_dn or not max_dn:
+      ( min_dn, max_dn ) = band.ComputeRasterMinMax( True )
 
-      if num > self.handle_.RasterCount:
-        sys.exit( "Index [{0}] out of bounds".format( num ) )
-        return None
+    info['min'] = int( min_dn )
+    info['max'] = int( max_dn )
 
-      info['nodata'] = self.GetBandNoData()
+    return info
 
-      band = self.handle_.GetRasterBand( num )
+  def ComputeBandHistogram( self, num, normalized = False ):
+    """
+    Compute histogram for a particular raster band
+    """
 
-      info['min'] = band.GetMinimum()
-      info['max'] = band.GetMaximum()
+    hist = {}
 
-      if not info['min'] or not info['max']:
-        ( info['min'], info['max'] ) = band.ComputeRasterMinMax( True )
+    band = self.LoadBand( num )
+    info = self.GetBandInfo( num )
 
-      return info
+    for dn in range( info['min'], info['max'] + 1 ):
+      hist[dn] = np.sum( band == dn )
 
-    def ComputeBandHistogram( self, num, normalized = False ):
-      """
-      Compute histogram for a particular raster band
-      """
+    pixels = 0.0
 
-      hist = {}
+    if info['nodata'] is None:
+      pixels = float( band.Xsize * band.Ysize )
+    else:
+      pixels = float( np.sum( band != info['nodata'] ) )
+      if hist.has_key( info['nodata'] ):
+        del hist[info['nodata']]
 
-      band = self.LoadBand( num )
-      info = self.GetBandInfo( num )
+    assert( pixels == sum( hist.values() ) )
 
-      for dn in range( info['min'], info['max'] + 1 ):
-        hist[dn] = np.sum( band == dn )
-
-      pixels = 0.0
-
-      if info['nodata'] is None:
-        pixels = float( band.XSize * band.YSize )
-      else:
-        pixels = np.sum( band != info['nodata'] )
-        if hist.has_key( info['nodata'] ):
-          del hist[info['nodata']]
-
-      if normalized:
-        for dn in hist.keys():
-          if hist[dn] == 0:
-            del hist[dn]
-          else:
-            hist[dn] /= pixels
-
-      return hist
-
-    def ComputeBandEntropy( self, num ):
-      """
-      Compute histogram for a particular raster band
-      """
-
-      hist = self.ComputeBandHistogram( num )
-
-      result = 0.0
-
+    if normalized:
       for dn in hist.keys():
-        p = hist[dn]
-        result += ( p * math.log( p, 2.0 ) )
+        if hist[dn] == 0:
+          del hist[dn]
+        else:
+          hist[dn] /= pixels
 
-      return abs( result )
+    return hist
+
+  def ComputeBandEntropy( self, num ):
+    """
+    Compute entropy for a particular raster band
+    """
+
+    hist = self.ComputeBandHistogram( num, True )
+
+    result = 0.0
+    for p in hist.values():
+      result += ( p * math.log( p, 2.0 ) )
+
+    return abs( result )
