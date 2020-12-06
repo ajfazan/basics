570a9988caaadaa0fb80543564ee878e1c95759f
ajfazan <ajfazan@gmail.com>
2020-12-06 13:12:33 -0300

    Tool 'GET_GNSS_FINAL_PRODUCTS' updated

--- scripts/get_gnss_final_products.sh
+++ scripts/get_gnss_final_products.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-if [ ${#} -ne 2 ]; then
+if [ ${#} -ne 1 ]; then
 
-  printf "Usage:\n\t%s <YEAR> <GPS_WEEK>\n" $(basename ${0})
+  printf "Usage:\n\t%s <GPS_WEEK>\n" $(basename ${0})
   exit 1
 
 fi
 
-# wget -c ftp://cddis.gsfc.nasa.gov/glonass/products/${2}/emx${2}[0-6].sp3.Z
+GPS_WEEK=$(printf "%04d" ${1})
 
-# wget -c ftp://cddis.gsfc.nasa.gov/pub/gps/products/mgex/${2}/com${2}[0-6].sp3.Z
+curl -u anonymous:ajfazan@gmail.com -O --ftp-ssl \
+  ftp://gdc.cddis.eosdis.nasa.gov/gnss/products/${GPS_WEEK}/igs${GPS_WEEK}[0-6].sp3.Z
 
-wget -c ftp://ftp.unibe.ch/aiub/CODE/${1}/COD${2}[0-6].ION.Z
+curl -u anonymous:ajfazan@gmail.com -O --ftp-ssl \
+  ftp://gdc.cddis.eosdis.nasa.gov/glonass/products/${GPS_WEEK}/igl${GPS_WEEK}[0-6].sp3.Z
